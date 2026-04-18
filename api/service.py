b7415c31f82905c945c787c72b6dec38398cf478
surajudeentoheeb19@gmail.com <gandii@DESKTOP-HL9QDCR.localdomain>
2026-04-18 07:37:50 -0700

    commit

--- api/service.py
+++ api/service.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core.cache import cache
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, APIView, staticmethod
+from rest_framework.decorators import api_view, APIView
 import requests
 #import datetime, timezone
 import asyncio
